86359ab3ec250dcebb2d3e6dfaa04f6fb658bf3c
Carlos Moncada Garcia <carlos.garcia@west-mec.org>
2015-09-16 16:29:38 -0700

    made changes to phpstorm

--- temp_converter.js
+++ temp_converter.js
@@ -1,12 +1,21 @@
 // Set this variable to a starting Fahrenheit temperature...
-var fahrenheit = 32;
 
+var fahrenheit = 32;
 // Enter conversion code below...
-var fahrenheitToCelsius = ...;
-var fahrenheitToKelvin = ...;
+var fahrenheitToCelsius = (32 - 32)/1.8 ;
+var fahrenheitToKelvin = 273.15= (32 + 459.67)/1.8;
 
 // Enter console.log statements below...
 
 // Repeat the above for Celsius and Kelvin...
+console.log  ("fahrenheit: "  + (fahrenheit));
+console.log ("celsius: " - (fahrenheitToCelsius ));
+console.log ("kelvin:" + (fahrenheitToKelvin));
 var celsius = 0;
 var kelvin = 273.15;
+
+var celsiusToFahrenheit = (32 = 0 * 1.8 + 32);
+var celsiusToKelvin = (273.15 = 0 + 273.15);
+
+var kelvinToFahrenheit = (32 = 273.15 * 1.8 - 459.67);
+var kelvinToCelsius = (0 = 273.15 - 273.15);
